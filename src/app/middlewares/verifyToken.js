a3bcbb6e588b6c8e5d201a2449128a146841d890
ramim-ahmed <ramimahmedraj2@gmail.com>
2024-06-02 01:28:12 +0600

    [DONE] veriyToken-middleware

---
+++ src/app/middlewares/verifyToken.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+const verifyToken = (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  const token = req.headers.authorization.split(" ")[1];
+  jwt.verify(token, process.env.access_token_secret, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
+module.exports = verifyToken;
