e5891118f80bc82a034ed6777d088324828fcdda
ramim-ahmed <ramimahmedraj2@gmail.com>
2024-06-01 21:16:56 +0600

    [DONE] user-service-functionality

--- src/app/modules/user/user.model.js
+++ src/app/modules/user/user.model.js
@@ -1,26 +1,34 @@
 const { Schema, model } = require("mongoose");
 
-const userSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      index: true,
+      unique: true,
+    },
+    photo: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["USER", "SELLER", "ADMIN"],
+      default: "USER",
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    index: true,
-    unique: true,
-  },
-  photo: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    enum: ["USER", "SELLER", "ADMIN"],
-    default: "USER",
-  },
-});
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    timestamps: true,
+  }
+);
 
 const User = model("User", userSchema);
 
---
+++ src/app/modules/user/user.service.js
@@ -0,0 +1,24 @@
+const User = require("./user.model");
+
+const createNewUser = async (data) => {
+  const { email } = data;
+  const isExits = await User.findOne({ email });
+  if (isExits) return isExits;
+  const result = await User.create(data);
+  return result;
+};
+
+const getAllUsers = async () => {
+  const result = await User.find({}).sort({ createdAt: "desc" });
+  return result;
+};
+
+const changeRole = async (email, role) => {
+  const result = await User.updateOne({ email }, { role });
+  return result;
+};
+module.exports.userService = {
+  createNewUser,
+  getAllUsers,
+  changeRole,
+};
